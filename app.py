ad46dce9ecd362d0b132dd53cb9eeb2ffdc88cce
Anthony Altieri <altiera@sunypoly.edu>
2023-11-21 20:54:31 -0500

    dash takes soooo much code

--- app.py
+++ app.py
@@ -1,10 +1,133 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
+import dash_bootstrap_components as dbc
+import pandas as pd
 
-app = Dash(__name__)
+# Initialize the app
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = html.Div([
-    html.Div(children="Hello World")
-])
+# Read in my data
+df = pd.read_csv('CrashData.csv')
+
+#Create our time options
+HOURS = [str(i) for i in range(1, 13)]
+MINUTES = [f"{i : 03d}" for i in range(0, 60)]
+AMPM = ['AM', 'PM']
+
+# App layout
+SIDEBAR_STYLE = {
+    'position': 'fixed',
+    'top': 0,
+    'left': 0,
+    'bottom': 0,
+    'width': '16rem',
+    'padding': '2rem 1rem',
+    'background-color': '#f8f9fa'
+}
+
+sidebar = html.Div(
+    [
+        dbc.Row(
+          html.Label('Start Time (HH:MM AM/PM)')  
+        ),
+        dbc.Row(
+            html.Div(
+                children=[
+                    html.Div(
+                        dcc.Dropdown(
+                            id='start_time_hour',
+                            options=HOURS,
+                            value='start_hour',
+                            placeholder='HH',
+                            clearable=True,
+                        ),
+                        style=dict(width='30%')
+                    ),
+                    html.Div(
+                        dcc.Dropdown(
+                            id='start_time_minute',
+                            options=MINUTES,
+                            value='start_min',
+                            placeholder='MM',
+                            clearable=True,
+                        ),
+                        style=dict(width='30%')
+                    ),
+                    html.Div(
+                        dcc.Dropdown(
+                            id='start_time_ampm',
+                            options=AMPM,
+                            value='start_ampm',
+                            placeholder='AM/PM',
+                            clearable=True,
+                        ),
+                        style=dict(width='40%')
+                    )
+                ],
+                style=dict(display='flex')
+            ), 
+        ),
+        dbc.Row(
+          html.Label('End Time (HH:MM AM/PM)')  
+        ),
+        dbc.Row(
+            html.Div(
+                children=[
+                    html.Div(
+                        dcc.Dropdown(
+                            id='end_time_hour',
+                            options=HOURS,
+                            value='end_hour',
+                            placeholder='HH',
+                            clearable=True,
+                        ),
+                        style=dict(width='30%')
+                    ),
+                    html.Div(
+                        dcc.Dropdown(
+                            id='end_time_minute',
+                            options=MINUTES,
+                            value='end_min',
+                            placeholder='MM',
+                            clearable=True,
+                        ),
+                        style=dict(width='30%')
+                    ),
+                    html.Div(
+                        dcc.Dropdown(
+                            id='end_time_ampm',
+                            options=AMPM,
+                            value='end_ampm',
+                            placeholder='AM/PM',
+                            clearable=True,
+                        ),
+                        style=dict(width='40%')
+                    )
+                ],
+                style=dict(display='flex')
+            ), 
+        )
+    ],
+    style=SIDEBAR_STYLE
+)
+
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+content = html.Div(
+    id="map",
+    style=CONTENT_STYLE
+)
+
+
+app.layout = html.Div(
+    [
+        sidebar,
+        content
+    ]
+)
 
 if __name__ == '__main__':
     app.run(debug=True)
