79806d670ae067ef7e0ba15dca68416f055e995b
Anthony Altieri <altiera@sunypoly.edu>
2023-11-23 14:58:34 -0500

    started on callbacks

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, dash_table, Input, Output
 import dash_bootstrap_components as dbc
 import pandas as pd
+import datetime
+from datetime import date
 
 # Initialize the app
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Read in my data
-df = pd.read_csv('CrashData.csv')
-
+df = pd.read_csv('CrashData.csv', index_col=0)
+formatting = '%m/%d/%Y %H:%M'
+df['IncidentDateTime'] = pd.to_datetime(df['IncidentDateTime'], format=formatting)
+df['IncidentDateTime'] = df['IncidentDateTime'].apply(lambda x: x.strftime("%m/%d/%Y %I:%M%p"))
 #Create our time options
-HOURS = [str(i) for i in range(1, 13)]
-MINUTES = [f"{i : 03d}" for i in range(0, 60)]
+HOURS = [f"{i:02d}" for i in range(1, 13)]
+MINUTES = [f"{i:02d}" for i in range(0, 60)]
 AMPM = ['AM', 'PM']
 
 # App layout
@@ -19,15 +23,21 @@ SIDEBAR_STYLE = {
     'top': 0,
     'left': 0,
     'bottom': 0,
-    'width': '16rem',
+    'width': '17rem',
     'padding': '2rem 1rem',
     'background-color': '#f8f9fa'
 }
 
 sidebar = html.Div(
-    [
+    children=[
+        html.H2("Filters"),
+        html.Hr(),
         dbc.Row(
-          html.Label('Start Time (HH:MM AM/PM)')  
+            html.H5("Time (HH:MM AM/PM)")
+        ),
+        #Start time input
+        dbc.Row(
+          html.Label('Start')  
         ),
         dbc.Row(
             html.Div(
@@ -36,7 +46,7 @@ sidebar = html.Div(
                         dcc.Dropdown(
                             id='start_time_hour',
                             options=HOURS,
-                            value='start_hour',
+                            value=None,
                             placeholder='HH',
                             clearable=True,
                         ),
@@ -46,7 +56,7 @@ sidebar = html.Div(
                         dcc.Dropdown(
                             id='start_time_minute',
                             options=MINUTES,
-                            value='start_min',
+                            value=None,
                             placeholder='MM',
                             clearable=True,
                         ),
@@ -56,7 +66,7 @@ sidebar = html.Div(
                         dcc.Dropdown(
                             id='start_time_ampm',
                             options=AMPM,
-                            value='start_ampm',
+                            value=None,
                             placeholder='AM/PM',
                             clearable=True,
                         ),
@@ -66,8 +76,9 @@ sidebar = html.Div(
                 style=dict(display='flex')
             ), 
         ),
+        #End time input
         dbc.Row(
-          html.Label('End Time (HH:MM AM/PM)')  
+          html.Label('End')  
         ),
         dbc.Row(
             html.Div(
@@ -76,7 +87,7 @@ sidebar = html.Div(
                         dcc.Dropdown(
                             id='end_time_hour',
                             options=HOURS,
-                            value='end_hour',
+                            value=None,
                             placeholder='HH',
                             clearable=True,
                         ),
@@ -86,7 +97,7 @@ sidebar = html.Div(
                         dcc.Dropdown(
                             id='end_time_minute',
                             options=MINUTES,
-                            value='end_min',
+                            value=None,
                             placeholder='MM',
                             clearable=True,
                         ),
@@ -96,7 +107,7 @@ sidebar = html.Div(
                         dcc.Dropdown(
                             id='end_time_ampm',
                             options=AMPM,
-                            value='end_ampm',
+                            value=None,
                             placeholder='AM/PM',
                             clearable=True,
                         ),
@@ -105,6 +116,27 @@ sidebar = html.Div(
                 ],
                 style=dict(display='flex')
             ), 
+        ),
+        html.Br(),
+        #Input date range
+        dbc.Row(
+            html.H5("Date")
+        ),
+        dbc.Row(
+            html.Div(
+                dcc.DatePickerRange(
+                    id='date_picker_range',
+                    min_date_allowed=date(2019, 1, 1),
+                    max_date_allowed=date(2022, 1, 30),
+                    month_format='M-D-Y',
+                    clearable=True
+                )
+            )
+        ),
+        dbc.Row(
+            html.Div(
+                id='filter_output'
+            )
         )
     ],
     style=SIDEBAR_STYLE
@@ -116,18 +148,49 @@ CONTENT_STYLE = {
     "padding": "2rem 1rem",
 }
 
-content = html.Div(
-    id="map",
+table = html.Div(
+    children=[
+        dash_table.DataTable(
+            id='table_data',
+            data=df.to_dict('records'),
+            #columns=[{"name": i, "id": i} for i in df.columns]
+        )
+    ],
     style=CONTENT_STYLE
 )
 
 
 app.layout = html.Div(
-    [
+    children=[
         sidebar,
-        content
+        table
     ]
 )
+
+@app.callback(
+    Output('filter_output', 'children'),
+    Input('start_time_hour', 'value'),
+    Input('start_time_minute', 'value'),
+    Input('start_time_ampm', 'value'),
+    Input('end_time_hour', 'value'),
+    Input('end_time_minute', 'value'),
+    Input('end_time_ampm', 'value'),
+    Input('date_picker_range', 'start_date'),
+    Input('date_picker_range', 'end_date')
+)
+def update_data(start_hr, start_min, start_ampm, end_hr, end_min, end_ampm, start_date, end_date):
+    
+    # Handles time input
+    # If there is a value in all time fields
+    if None not in (start_hr, start_min, start_ampm, end_hr, end_min, end_ampm):
+        start_time_input = f"{start_hr}:{start_min}{start_ampm}"
+        end_time_input = f"{end_hr}:{end_min}{end_ampm}"
+    if (start_date is not None) and (end_date is not None):
+        pass
+    return "TESTING"
+        
+    
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+#Just using this file to prep the csv file
