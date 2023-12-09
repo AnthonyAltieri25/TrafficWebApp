f3d75185854307341bbf4ca2f7f6d1074d497630
Anthony Altieri <altiera@sunypoly.edu>
2023-12-09 13:47:58 -0500

    got compiled data file

--- data.py
+++ data.py
@@ -1,19 +1,47 @@
 import pandas as pd
 import pyarrow
-import io
+import os
 
-#Extract the info I want out of the dicts in the df
+# Extract the info I want out of the dicts in the df
 def extract_value(data:dict, key:str):
     return data.get(key)
 
-#Just using this file to play with the data
-test_data = pd.read_parquet('wejo\part-00007-tid-3851099292331252689-9e71c5e6-c05e-4508-b8c6-abb52c06c814-9855-1.c000.gz.parquet', engine='pyarrow')
-test_data = test_data.iloc[0:5]
-test_data.drop(columns=['journeyId', 'status', 'event'], axis=1, inplace=True)
-test_data['latitude'] = test_data['location'].apply(lambda x: extract_value(x, 'latitude'))
-print(test_data.size)
-print(test_data.columns)
-print(test_data.head())
-print(test_data.iloc[0]['location'].get('latitude'))
+def check_same_headings():
+    previous = None
+    # Loop through each data file
+    for i in os.listdir('wejo/2022-08-19'):
+        for j in os.listdir('wejo/2022-08-19/' + i):
+            # Get each file
+            df = pd.read_parquet('wejo/2022-08-19/' + i + '/' + j, engine='pyarrow')
+            if previous is not None and previous != df.columns.to_list():
+                # Extract the data I want
+                print('Differing data header at wejo/2022-08-19/' + i + '/' + j)
+                break
+            else:
+                previous = df.columns.to_list()
+    print(f'All data has same headings: {previous}')
 
-test_data.to_csv('TEST.csv')
+def combine_data():
+    columns = ['time', 'speed', 'latitude', 'longitude']
+
+    my_data = pd.DataFrame(columns=columns)
+    # Loop through each data file
+    for i in os.listdir('wejo/2022-08-19'):
+        for j in os.listdir('wejo/2022-08-19/' + i):
+            # Get each file
+            df = pd.read_parquet('wejo/2022-08-19/' + i + '/' + j, engine='pyarrow')
+            temp = pd.DataFrame(columns=columns)
+            # Pull out the data I want
+            temp['time'] = df['capturedTimestamp']
+            temp['speed'] = df['metrics'].apply(lambda x: extract_value(x, 'speed'))
+            temp['latitude'] = df['location'].apply(lambda x: extract_value(x, 'latitude'))
+            temp['longitude'] = df['location'].apply(lambda x: extract_value(x, 'longitude'))
+            # Add the data to the running list
+            my_data = pd.concat([my_data, temp], ignore_index=True)
+            
+    my_data.to_parquet('TotalDataAug19.parquet', engine='pyarrow')
+
+df = pd.read_parquet('wejo/TotalDataAug19.parquet', engine='pyarrow')
+print(df.tail(10))
+print(df['speed'].max())
+print('Zeroes in speed:', (df['speed'] == 0).sum())
